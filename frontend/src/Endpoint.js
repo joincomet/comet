8b21a0bf3916bb5ddf2d8946d9e92ffe4be3c5a3
danbeneventano <danbeneventano@protonmail.com>
2020-10-28 21:12:41 -0600

    Fix staging endpoint

--- frontend/src/Endpoint.js
+++ frontend/src/Endpoint.js
@@ -1,6 +1,7 @@
-export const ENDPOINT = () =>
-  typeof window === 'undefined'
-    ? `http://${process.env.NEXT_STAGING === 'true' ? 'api-staging' : 'api'}:4000/graphql`
-    : process.env.NODE_ENV === 'production'
-    ? '/graphql'
-    : 'http://localhost:4000/graphql'
+export const ENDPOINT = () => {
+  if (typeof window === 'undefined') {
+    return process.env.NEXT_STAGING === 'true' ? 'http://api-staging:4001/graphql' : 'http://api:400/graphql'
+  } else {
+    return process.env.NODE_ENV === 'production' ? `https://${new URL(location.href).hostname}/graphql` : 'http://localhost:4000/graphql'
+  }
+}
