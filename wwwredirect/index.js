1eb59bfd078276c5f59a4ebe9b24327e77ed557a
danbeneventano <danbeneventano@protonmail.com>
2020-12-01 13:55:01 -0500

    add www redirect

---
+++ wwwredirect/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const url = require("url");
+
+http
+  .createServer(function (req, res) {
+    const pathname = url.parse(req.url).pathname;
+    res.writeHead(301, { Location: "https://cometx.io/" + pathname });
+    res.end();
+  })
+  .listen(8080);
