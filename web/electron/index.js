df30d20e58f36ff1a0efd24c448de82182d5b68f
Dan Beneventano <danbeneventano@protonmail.com>
2021-04-10 01:36:45 -0600

    fix prod electron error

--- web/electron/index.js
+++ web/electron/index.js
@@ -4,11 +4,29 @@ const { join } = require('path')
 const { BrowserWindow, app, Tray, Menu } = require('electron')
 const isDev = require('electron-is-dev')
 const contextMenu = require('electron-context-menu')
-const { log } = require('./log')
+const log = require('electron-log')
 const { createLoadingScreen } = require('./loading')
-const { runUpdater } = require('./update')
 const { createWindow } = require('./mainWindow')
 const DiscordRPC = require('discord-rpc')
+const { autoUpdater } = require('electron-updater')
+const ProgressBar = require('electron-progressbar')
+
+autoUpdater.logger = log
+autoUpdater.logger.transports.file.level = 'info'
+autoUpdater.autoDownload = false
+
+if (!isDev) {
+  log.transports.file.resolvePath = variables => {
+    return join(variables.electronDefaultDir, variables.fileName)
+  }
+}
+
+autoUpdater.checkForUpdates()
+
+// Check for updates every minute
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 1000 * 60)
 
 app.setAppUserModelId('Comet')
 
@@ -16,14 +34,101 @@ log.info('App starting...')
 
 contextMenu({ showInspectElement: true }) // TODO disable this
 
+let progressBar = null
 let mainWindow = null
 let loadingScreen = null
 let tray = null
 
 app.whenReady().then(() => {
   loadingScreen = createLoadingScreen()
-  if (!isDev) runUpdater(mainWindow, loadingScreen)
-  else {
+  if (!isDev) {
+    autoUpdater.on('checking-for-update', () => {
+      log.info('Checking for update...')
+    })
+
+    autoUpdater.on('update-available', info => {
+      log.info('Update available.')
+      log.info(info)
+
+      if (mainWindow) {
+        // Show icon in main app
+        mainWindow.send('updateAvailable')
+        return
+      }
+
+      autoUpdater.downloadUpdate()
+
+      progressBar = new ProgressBar({
+        indeterminate: false,
+        title: 'Update in progress',
+        text: 'Preparing to download updates.',
+        detail: 'Downloading...',
+        browserWindow: {
+          webPreferences: {
+            nodeIntegration: true
+          }
+        }
+      })
+
+      progressBar
+        .on('completed', function () {
+          progressBar.detail = 'Download completed.'
+        })
+        .on('aborted', function (value) {
+          log.error('Download aborted.')
+          app.quit()
+        })
+        .on('progress', function (value) {
+          progressBar.text = 'Downloading updates.'
+          progressBar.detail = `Downloading... ${value}%`
+        })
+    })
+
+    autoUpdater.on('update-not-available', info => {
+      log.info('Update not available.')
+      log.info(info)
+      mainWindow = createWindow()
+      mainWindow.webContents.on('did-finish-load', () => {
+        if (loadingScreen) {
+          loadingScreen.hide()
+        }
+        mainWindow.show()
+        mainWindow.send('windowOpened')
+      })
+    })
+
+    autoUpdater.on('error', err => {
+      log.error('Error in auto-updater. ' + err)
+      mainWindow = createWindow()
+      mainWindow.webContents.on('did-finish-load', () => {
+        if (loadingScreen) {
+          loadingScreen.hide()
+        }
+        mainWindow.show()
+        mainWindow.send('windowOpened')
+      })
+    })
+
+    autoUpdater.on('download-progress', progressObj => {
+      let logMessage = 'Download speed: ' + progressObj.bytesPerSecond
+      logMessage = logMessage + ' - Downloaded ' + progressObj.percent + '%'
+      logMessage =
+        logMessage +
+        ' (' +
+        progressObj.transferred +
+        '/' +
+        progressObj.total +
+        ')'
+      log.info(logMessage)
+      progressBar.value = parseInt(progressObj.percent)
+    })
+
+    autoUpdater.on('update-downloaded', info => {
+      log.info('Update downloaded')
+      log.info(info)
+      setImmediate(() => autoUpdater.quitAndInstall())
+    })
+  } else {
     mainWindow = createWindow(loadingScreen)
     mainWindow.webContents.on('did-finish-load', () => {
       if (loadingScreen) {
