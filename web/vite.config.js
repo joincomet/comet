d3d9cd73304fa647f0f5e5dd53d9a4da6a800d64
Sean Norwood <norwood.sean@gmail.com>
2021-06-10 10:55:18 -0500

    chore: add manifest content

--- web/vite.config.js
+++ web/vite.config.js
@@ -7,7 +7,37 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig(({ command }) => ({
   base: process.env.ELECTRON === 'true' ? './' : '/',
   plugins: [reactRefresh(), VitePWA({
-    manifest: {},
+    manifest: {
+      "theme_color": "#f69435",
+      "background_color": "#f69435",
+      "display": "browser",
+      "scope": "/",
+      "start_url": "/",
+      "name": "Comet",
+      "short_name": "Comet",
+      "icons": [
+        {
+          "src": "/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    },
     workbox: {}
   })],
   resolve: {
