0160e4817e017e86bb23c418958d81a133f69202
OwaisSiddiqui <GitHubGIt18888*>
2021-07-12 10:15:18 -0400

    Remove unecessary position

--- web/src/components/server/ServerInfoCard.jsx
+++ web/src/components/server/ServerInfoCard.jsx
@@ -25,7 +25,7 @@ export default function ServerInfoCard({
     >
       <Link
         to={`/+${server.name}`}
-        className={`${className} relative relative flex flex-col w-full rounded-lg group dark:bg-gray-800 dark:hover:bg-gray-850 duration-200 transform transition hover:shadow-xl bg-white ${
+        className={`${className} relative flex flex-col w-full rounded-lg group dark:bg-gray-800 dark:hover:bg-gray-850 duration-200 transform transition hover:shadow-xl bg-white ${
           shadow ? 'shadow-lg' : ''
         }`}
       >
